a3ec8ba5490316c657163396f2f486ba2d2a649b
stevetorres705 <headintheclouds706@gmail.com>
2024-07-26 19:21:48 -0400

    Update Additive Program.py

--- Additive Program.py
+++ Additive Program.py
@@ -9,9 +9,7 @@ If the user enters 5, the answer should be 15 based on the logic (1+2+3+4+5=15).
 
 # DAFT PUNK
 def addem(n):
-    global x
     x = 0
-    global y
     y = 0
     while x != n:
         x+=1
@@ -19,8 +17,6 @@ def addem(n):
     return y
 
 # MEAT
-
 n = int(input("Enter the number you want to go to: "))
-addem(n)
-print("The sum of", n,"consecutive numbers starting at 1 is", y)
 
+print("The sum of", n,"consecutive numbers starting at 1 is", addem(n))
